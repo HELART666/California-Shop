a233c8b1b9d600250af417692f58cd3970e92df7
HELART666 <helart.beats266@gmail.com>
2022-06-26 21:51:09 +0400

    The header and the first page with a slider.

---
+++ js/script.js
@@ -0,0 +1,47 @@
+new Swiper('.swiper-container', {
+    loop: true,
+    autoplay:{
+        delay: 1500,
+        stopOnLastSlide: false,
+        disableOnInteraction: false,
+    },
+    speed: 500,
+});
+const mobileMenuButton = document.querySelectorAll('.menu__mobile');
+const modal = document.querySelector('.modal__window__container');
+const modalList = document.querySelectorAll('.modal__item');
+const modalLink = document.querySelectorAll('.modal__link');
+
+
+document.querySelector('.menu__mobile').addEventListener('click', function(){
+    modal.classList.remove("hidden");
+    let i = 0;
+    let id = setInterval(() => {   
+        modalList[i].classList.remove("hidden__list"); 
+        modalLink[i].classList.add("open"); 
+        i++; 
+        if(i === modalList.length){
+            clearInterval(id);
+        }
+    }, 200);
+    i = 0;
+});
+
+
+
+
+document.querySelector('.menu__close').addEventListener('click', function(){
+    modal.classList.add("hidden");
+    let i = 3;
+    let id = setInterval(() => {   
+        modalList[i].classList.add("hidden__list"); 
+        modalLink[i].classList.remove("open"); 
+        i--; 
+        if(i == 0){
+            modalList[i].classList.add("hidden__list"); 
+            modalLink[i].classList.remove("open"); 
+            clearInterval(id);
+        }
+    }, 200);
+    i = 3;
+});
