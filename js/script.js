86843c4126b0ab3f50dfa11cc716c40895d3cde6
HELART666 <helart.beats266@gmail.com>
2022-07-09 08:49:09 +0400

    penultimate version

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ new Swiper('.swiper-container', {
         clickable: true,
     },
 });
+
 const mobileMenuButton = document.querySelectorAll('.menu__mobile');
 const modal = document.querySelector('.modal__window__container');
 const modalList = document.querySelectorAll('.modal__item');
@@ -25,8 +26,16 @@ const buttonValue = document.querySelectorAll('.else__fastsearch_button');
 const searchInput = document.querySelector('.search__input');
 const list = document.querySelectorAll('.list__items');
 const content = document.querySelectorAll('.content');
-const availableScreenWidth = window.screen.availWidth;
+const search = document.querySelector('#search');
+const searchMenu = document.querySelector('.menu__input');
+const logo = document.querySelector('.header__logo');
+const listImage = document.querySelectorAll('.collap__img');
+const headerMenu = document.querySelector('.menu__list');
+const coll = document.querySelectorAll(".collapsible");
+const listFooter = document.querySelectorAll('.list__footer');
 
+let availableScreenWidth;
+let outerScreenWidth;
 document.querySelector('.menu__mobile').addEventListener('click', function(){
     modal.classList.remove("hidden");
     swiperControls.classList.add("hidden");
@@ -62,14 +71,18 @@ document.querySelector('.menu__close').addEventListener('click', function(){
 function addNum(el) {
     searchInput.value = el.textContent;
 }
+
+
 window.addEventListener("resize", function() {
+    availableScreenWidth = window.screen.availWidth;
+    searchMenu.style.maxWidth = 0 + "px";
     if (window.matchMedia("(max-width: 975px)").matches) {
         for(let i = 0; i < content.length; i++){
             content[i].style.maxHeight = null;
            }
-           let coll = document.querySelectorAll(".collapsible");
         for (let i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function() {
+            listImage[i].classList.toggle('rotate');
             this.classList.toggle("active");
             let content = this.nextElementSibling;
             if (content.style.maxHeight){
@@ -90,6 +103,62 @@ window.addEventListener("resize", function() {
 
 
 
+
+search.addEventListener('click', function(){
+    availableScreenWidth = window.screen.availWidth;
+    if(availableScreenWidth < 1136){
+        headerMenu.classList.toggle("logo__hidden");
+    }
+    if(availableScreenWidth < 625){
+        logo.classList.toggle("logo__hidden");
+    }
+    if(searchMenu.style.maxWidth == 200 + "px"){
+        searchMenu.style.maxWidth = 0 + "px";
+    } else{
+        searchMenu.style.maxWidth = 200 + "px";
+    }
+});
+
+window.onscroll = function (e) {  
+    searchMenu.style.maxWidth = 0 + "px";
+} 
+
 buttonValue.forEach(div => { 
       div.addEventListener('click', () => addNum(div));
 });
+
+
+let ro = new ResizeObserver(entries => {
+    for (let entry of entries) {
+      const cr = entry.contentRect;
+    //   console.log('Element:', entry.target);
+    //   console.log(`Element size: ${cr.width}px x ${cr.height}px`);
+    //   console.log(`Element padding: ${cr.top}px ; ${cr.left}px`);
+      availableScreenWidth = window.screen.availWidth;
+            searchMenu.style.maxWidth = 0 + "px";
+            if (window.matchMedia("(max-width: 975px)").matches) {
+                for(let i = 0; i < content.length; i++){
+                    content[i].style.maxHeight = null;
+                   }
+                for (let i = 0; i < coll.length; i++) {
+                coll[i].addEventListener("click", function() {
+                    listImage[i].classList.toggle('rotate');
+                    this.classList.toggle("active");
+                    let content = this.nextElementSibling;
+                    if (content.style.maxHeight){
+                    content.style.maxHeight = null;
+                    } else {
+                    content.style.maxHeight = content.scrollHeight + "px";
+                    } 
+                });
+        }
+            } else {
+                for(let i = 0; i < content.length; i++){
+                    content[i].style.maxHeight = "147" + "px";
+                   }
+            }
+    }
+  });
+  
+ro.observe(coll[0]);
+
